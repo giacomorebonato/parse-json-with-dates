5fa2d013de1369bec3e7720a0bd16b2b6c74c6d0
Giacomo Rebonato <grebonato@outlook.ie>
2021-01-06 11:29:49 +0100

    Fixing rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,21 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
 import typescript from 'rollup-plugin-typescript2'
 
-export default [
-  {
-    input: './src/index.ts',
-    output: {
-      file: './lib/index.esm.js',
-      format: 'esm',
-      name: 'parse-json-with-dates',
-    },
-    plugins: [typescript(), nodeResolve()],
-  },
+const config =  [
   {
     input: './src/index.ts',
     output: {
       file: './lib/index.js',
+      format: 'umd',
       name: 'parse-json-with-dates',
     },
     plugins: [typescript(), nodeResolve()],
   },
 ]
+
+if (process.env.NODE_ENV === 'production') {
+  config[0].plugins.push(terser())
+}
+
+export default config
